3bba188fcc7d10d3dbebbbe08f56f229ef455125
Anton Holmes <holmesanton@icloud.com>
2018-08-17 01:00:05 -0400

    fix(build): corrected errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import {createBrowserHistory} from 'history'
 import {Router, Route, Switch} from 'react-router'
 import './index.css'
-import App from './app'
 import registerServiceWorker from './registerServiceWorker'
 import indexRoutes from './routes'
 
